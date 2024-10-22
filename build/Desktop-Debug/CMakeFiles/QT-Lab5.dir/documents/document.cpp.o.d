2b06e1ed34fd5283c3ae5c8f58260d4e3d506c80
PorterCat <leportercat@gmail.com>
2024-10-22 15:35:37 +0700

    Change .gitignore

--- build/Desktop-Debug/CMakeFiles/QT-Lab5.dir/documents/document.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/QT-Lab5.dir/documents/document.cpp.o: \
- /home/portercat/Programming/QT-Lab5/documents/document.cpp \
- /usr/include/stdc-predef.h \
- /home/portercat/Programming/QT-Lab5/documents/document.h
--- build/Desktop-Debug/CMakeFiles/QT-Lab5.dir/documents/table_document.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/QT-Lab5.dir/documents/table_document.cpp.o: \
- /home/portercat/Programming/QT-Lab5/documents/table_document.cpp \
- /usr/include/stdc-predef.h \
- /home/portercat/Programming/QT-Lab5/documents/table_document.h
